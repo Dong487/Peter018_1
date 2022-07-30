7476cb9af987f64882d51e32c4be73db68308dbc
Dong487 <Dsheng7777777@gmail.com>
2022-07-30 11:52:52 +0800

    Initial Commit

---
+++ Peter018_1/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Peter018_1
+//
+//  Created by DONG SHENG on 2022/7/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Peter018_1/Peter018_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Peter018_1App.swift
+//  Peter018_1
+//
+//  Created by DONG SHENG on 2022/7/30.
+//
+
+import SwiftUI
+
+@main
+struct Peter018_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
